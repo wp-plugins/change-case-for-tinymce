381ab91da21d63f47f34ebb156f116b3c08e78e7
MasterK <MasterK@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-15 17:08:51 +0000

    Fix Options page
    git-svn-id: https://plugins.svn.wordpress.org/change-case-for-tinymce/trunk@756782 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- change-case.php
+++ change-case.php
@@ -3,7 +3,7 @@
  * Plugin name: Change Case
  * Author: Michael Aronoff
  * Version: 1.1
- * Description: Adds in [uc] [lc] [tc] shortcodes to use changing text case in posts and pages.
+ * Description: Adds Change Case adds buttons to change text case in the WordPress visual editor.
  * */
 
 if ( !class_exists( "change_case" ) ):
