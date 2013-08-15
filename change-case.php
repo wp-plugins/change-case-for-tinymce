737cc13a8561b705b957e25f8c774eb4e2dbb4bb
MasterK <MasterK@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-15 16:04:25 +0000

    First Commit
    git-svn-id: https://plugins.svn.wordpress.org/change-case-for-tinymce/trunk@756759 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ change-case.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Plugin name: Change Case
+ * Author: Michael Aronoff
+ * Version: 1.0
+ * Description: Adds in [uc] [lc] [tc] shortcodes to use changing text case in posts and pages.
+ * */
+
+if ( !class_exists( "change_case" ) ):
+
+  /**
+   * Change Case
+   */
+  class change_case {
+
+  function __construct() {
+    define( "CC_URL", WP_PLUGIN_URL.'/'.str_replace( basename( __FILE__ ), "", plugin_basename( __FILE__ ) ) );
+    define( "CC_PLUGIN_DIR", "change-case" );
+    define( "CC_PLUGIN_URL", get_bloginfo( 'url' )."/wp-content/plugins/" . CC_PLUGIN_DIR );
+    add_shortcode( 'allcaps', array( __CLASS__, 'insert_all_caps' ) );
+    add_shortcode( 'nocaps', array( __CLASS__, 'insert_no_caps' ) );
+    add_shortcode( 'titlecase', array( __CLASS__, 'insert_title_case' ) );
+    register_activation_hook( __FILE__, array( __CLASS__, "register" ) );
+    add_action( 'init', array( __CLASS__, 'add_button' ) );
+    add_filter( 'tiny_mce_version', array( __CLASS__, 'refresh_mce' ) );
+    add_action( 'admin_menu', array( __CLASS__, 'menu' ) );
+  }
+
+  /* TINY MCE */
+
+  function register() {
+    $values= array( "ac"=>1, "nc"=>1, "tc"=>1 );
+    if ( get_option( "CC_HR_OPTIONS" ) ) {
+      $current = get_option( "CC_HR_OPTIONS" );
+      if ( is_serialized( $current ) ) {
+        $current = unserialize( $current );
+      }
+      $values = array_merge( $values, $current );
+      update_option( "CC_HR_OPTIONS", $values );
+    }
+    else {
+      add_option( "CC_HR_OPTIONS", $values );
+    }
+  }
+
+  function add_button() {
+    if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) )
+      return;
+    if ( get_user_option( 'rich_editing' ) == 'true' ) {
+      add_filter( 'mce_external_plugins', array( __CLASS__, 'add_tinymce_plugin' ) );
+      add_filter( 'mce_buttons', array( __CLASS__, 'register_button' ) );
+    }
+  }
+
+  function register_button( $buttons ) {
+    $current = get_option( "CC_HR_OPTIONS" );
+    if ( is_serialized( $current ) ) {$current = unserialize( $current );}
+    array_push( $buttons, "|" );
+    if ( $current['ac'] == 1 ) {
+      array_push( $buttons,  "allcaps" );
+    }
+    if ( $current['nc'] == 1 ) {
+      array_push( $buttons,  "nocaps" );
+    }
+    if ( $current['tc'] == 1 ) {
+      array_push( $buttons,  "titlecase" );
+    }
+    return $buttons;
+  }
+
+  function add_tinymce_plugin( $plugin_array ) {
+    $current = get_option( "CC_HR_OPTIONS" );
+    if ( is_serialized( $current ) ) {
+      $current = unserialize( $current );
+    }
+    if ( $current['ac'] == 1 ) {
+      $plugin_array['allcaps'] = CC_PLUGIN_URL . '/cc.js';
+    }
+    if ( $current['nc'] == 1 ) {
+      $plugin_array['nocaps'] = CC_PLUGIN_URL . '/cc.js';
+    }
+    if ( $current['tc'] == 1 ) {
+      $plugin_array['titlecase'] = CC_PLUGIN_URL . '/cc.js';
+    }
+    return $plugin_array;
+  }
+
+  function refresh_mce( $ver ) {
+    $ver += 7;
+    return $ver;
+  }
+
+  function menu() {
+    add_submenu_page( 'options-general.php', 'Change Case', 'Change Case', 'edit_posts', 'change-case', array( __CLASS__, 'options' ) );
+  }
+
+  function options() {
+    $current = get_option( "CC_HR_OPTIONS" );
+    if ( is_serialized( $current ) ) {$current = unserialize( $current );}
+?>
+    <div class='wrap'>
+      <div style='float:left; width:450px;'>
+        <h1>Change Case provided by Michael Aronoff of <a href="http://www.ciic.com" target="_blank">CIIC</a></h1>
+        <p class="description">The options below are to choose which buttons are added to the tinyMCE editor.</p>
+        <form method="post" action="options.php">
+          <?php wp_nonce_field( 'update-options' ); ?>
+          <table class="form-table">
+            <tr valign="top">
+              <th scope="row"><label for="CC_HR_OPTIONS['ac']">All Caps: </label></th>
+              <td><input type="checkbox" name="CC_HR_OPTIONS[cl]" id="CC_HR_OPTIONS['ac']" value='1' <?php if ( $current['ac']==1 ) {echo "checked='checked'";}?> /></td>
+            </tr>
+            <tr valign="top">
+              <th scope="row"><label for="CC_HR_OPTIONS['nc']">No Caps: </label></th>
+              <td><input type="checkbox" name="CC_HR_OPTIONS[cr]" id="CC_HR_OPTIONS['nc']" value='1' <?php if ( $current['nc']==1 ) {echo "checked='checked'";}?> /></td>
+            </tr>
+            <tr valign="top">
+              <th scope="row"><label for="CC_HR_OPTIONS['tc']">Title Case: </label></th>
+              <td><input type="checkbox" name="CC_HR_OPTIONS[cb]" id="CC_HR_OPTIONS['tc']" value='1' <?php if ( $current['tc']==1 ) {echo "checked='checked'";}?> /></td>
+            </tr>
+          </table>
+          <input type="hidden" name="action" value="update" />
+          <input type="hidden" name="page_options" value="CC_HR_OPTIONS" />
+          <p class="submit">
+            <input type="submit" class="button-primary" value="<?php _e( 'Save Changes' ) ?>" />
+          </p>
+        </form>
+      </div>
+    </div>
+<?php
+  }
+}
+
+$change_case = new change_case();
+endif;
+?>
---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== Change Text Case ===
+Contributors: MasterK
+Tags: text, case, TinyMCE, editor
+Requires at least: 3.3
+Tested up to: 3.6
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Change Case adds buttons to change text case in the WordPress visual editor.
+
+== Description ==
+
+This plugin solves one of my greatest annoyances. Quickly changing the case of selected text. Back when I built sites in Dreamweaver I had an extension that added this function so I could click a button and change the case of text but when I started developing for WordPress there was no similar plugin. So I wrote one!
+
+== Usage ==
+
+Highlight the text to be changed. Click the Visual Ediotr button inside your post/page for the desired effect:
+
+* **UC** - Changes selected text to all Uppercase.
+* **lc** - Changes selected text to all lowercase.
+* **Tc** - Changes selected text to all Title case.
+
+== Installation ==
+
+1. Upload the plugin to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add shortcode to your post or page where map should appear.
+
+== FAQ ==
+
+= Q. Where can the plugin be used? =
+The plugin can be used in the WordPress Visual Editor
+
+== Change Log ==
+= 1.0 =
+Initial Release
